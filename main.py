1a56c75f81a4c377c5d3fd688ea376456f33e50c
Lazar Jelic <lazarjelic98@gmail.com>
2020-07-19 22:21:06 +0200

    Added main, Added runner skeleton

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+import argparse
+from graphviz import Source
+from src.token import Class, Token
+from src.lexer import Lexer
+from src.parser import Parser
+from src.grapher import Grapher
+from src.runner import Runner
+
+RUN = False
+
+
+def handle_input(text):
+    lexer = Lexer(text)
+    parser = Parser(lexer)
+    grapher = Grapher(parser)
+    runner = Runner(parser)
+
+    if RUN:
+        return runner.run()
+    else:
+        return grapher.graph()
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('path')
+
+    args = arg_parser.parse_args()
+    path = args.path
+
+    with open(path, 'r') as input:
+        result = handle_input(input.read())
+
+    if not RUN:
+        dirs = os.path.splitext(path)[0].split(os.sep)
+        dirs.insert(-1, 'graph')
+        graph = os.sep.join(dirs)
+
+        s = Source(result, filename=graph, format='png')
+        s.view()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/runner.py
@@ -0,0 +1,82 @@
+from src.visitor import Visitor
+
+
+class Runner(Visitor):
+    def __init__(self, parser):
+        self.parser = parser
+
+    def visit_Program(self, parent, node):
+        pass
+
+    def visit_Decl(self, parent, node):
+        pass
+
+    def visit_ArrayDecl(self, parent, node):
+        pass
+
+    def visit_ArrayElem(self, parent, node):
+        pass
+
+    def visit_Assign(self, parent, node):
+        pass
+
+    def visit_If(self, parent, node):
+        pass
+
+    def visit_While(self, parent, node):
+        pass
+
+    def visit_For(self, parent, node):
+        pass
+
+    def visit_FuncDecl(self, parent, node):
+        pass
+
+    def visit_FuncImpl(self, parent, node):
+        pass
+
+    def visit_FuncCall(self, parent, node):
+        pass
+
+    def visit_Block(self, parent, node):
+        pass
+
+    def visit_Params(self, parent, node):
+        pass
+
+    def visit_Args(self, parent, node):
+        pass
+
+    def visit_Break(self, parent, node):
+        pass
+
+    def visit_Continue(self, parent, node):
+        pass
+
+    def visit_Return(self, parent, node):
+        pass
+
+    def visit_Type(self, parent, node):
+        pass
+
+    def visit_Int(self, parent, node):
+        pass
+
+    def visit_Char(self, parent, node):
+        pass
+
+    def visit_String(self, parent, node):
+        pass
+
+    def visit_Id(self, parent, node):
+        pass
+
+    def visit_BinOp(self, parent, node):
+        pass
+
+    def visit_UnOp(self, parent, node):
+        pass
+
+    def run(self):
+        program = self.parser.program()
+        self.visit(None, program)
