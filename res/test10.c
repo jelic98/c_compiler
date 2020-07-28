05ffbd85f53e1bea950cb680ca4551ee66847a24
Lazar Jelic <lazarjelic98@gmail.com>
2020-07-28 15:24:55 +0200

    Modified example 10

--- res/test10.c
+++ res/test10.c
@@ -1,10 +1,19 @@
 int main() {
 	char asd[50] = {'q', 'w', 'e', 'r', 't', 'y'};
 	char efg[50];
+	
+	printf("Unesite string: ");
 	scanf("%s", efg);
+	
+	printf("Konkatenacija stringova: %s + %s\n", asd, efg);
+	
 	strcat(asd, efg);
+	
 	int length;
 	length = strlen(asd);
-	printf("String of length %d is %s\n", length, asd);
+	
+	printf("Rezultat konkatenacije: %s\n", asd);
+	printf("Duzina konkatenacije: %d\n", length);
+	
 	return 0;
 }
