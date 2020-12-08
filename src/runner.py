c3ab287eb9e0daee4f386565d4916bca25d7e984
Lazar Jelic <lazarjelic98@gmail.com>
2020-12-08 20:28:45 +0100

    Updated Runner - v2

--- src/runner.py
+++ src/runner.py
@@ -10,13 +10,17 @@ class Runner(Visitor):
         self.global_ = {}
         self.local = {}
         self.scope = []
+        self.call_stack = []
+        self.search_new_call = True
         self.return_ = False
 
     def get_symbol(self, node):
+        recursion = self.is_recursion()
+        ref = -2 if recursion and not self.search_new_call else -1
         id_ = node.value
         for scope in reversed(self.scope):
             if scope in self.local:
-                curr_scope = self.local[scope][-1]
+                curr_scope = self.local[scope][ref]
                 if id_ in curr_scope:
                     return curr_scope[id_]
         return self.global_[id_]
@@ -33,6 +37,15 @@ class Runner(Visitor):
         scope = id(node)
         self.local[scope].pop()
 
+    def is_recursion(self):
+        if len(self.call_stack) > 0:
+            curr_call = self.call_stack[-1]
+            prev_calls = self.call_stack[:-1]
+            for call in reversed(prev_calls):
+                if call == curr_call:
+                    return True
+        return False
+
     def visit_Program(self, parent, node):
         for s in node.symbols:
             self.global_[s.id_] = s.copy()
@@ -44,10 +57,20 @@ class Runner(Visitor):
         id_.value = None
 
     def visit_ArrayDecl(self, parent, node):
-        pass
+        id_ = self.get_symbol(node.id_)
+        id_.symbols = node.symbols
+        size, elems = node.size, node.elems
+        if elems is not None:
+            self.visit(node, elems)
+        elif size is not None:
+            for i in range(size.value):
+                id_.symbols.put(i, id_.type_, None)
+                id_.symbols.get(i).value = None
 
     def visit_ArrayElem(self, parent, node):
-        pass
+        id_ = self.get_symbol(node.id_)
+        index = self.visit(node, node.index)
+        return id_.symbols.get(index.value)
 
     def visit_Assign(self, parent, node):
         id_ = self.visit(node, node.id_)
@@ -91,9 +114,11 @@ class Runner(Visitor):
         id_.params = node.params
         id_.block = node.block
         if node.id_.value == 'main':
+            self.call_stack.append(node.id_.value)
             self.init_scope(node.block)
             self.visit(node, node.block)
             self.clear_scope(node.block)
+            self.call_stack.pop()
 
     def visit_FuncCall(self, parent, node):
         func = node.id_.value
@@ -108,11 +133,18 @@ class Runner(Visitor):
                     format_ = format_.replace('%c', chr(a.value), 1)
                 elif isinstance(a, String):
                     format_ = format_.replace('%s', a.value, 1)
-                elif isinstance(a, Id):
+                elif isinstance(a, Id) or isinstance(a, ArrayElem):
                     id_ = self.visit(node.args, a)
-                    value = id_.value
-                    if id_.type_ == 'char':
-                        value = chr(value)
+                    if hasattr(id_, 'symbols') and id_.type_ == 'char':
+                        elems = id_.symbols
+                        ints = [s.value for s in elems]
+                        non_nulls = [i for i in ints if i is not None]
+                        chars = [chr(i) for i in non_nulls]
+                        value = ''.join(chars)
+                    else:
+                        value = id_.value
+                        if id_.type_ == 'char':
+                            value = chr(value)
                     format_ = re.sub('%[dcs]', str(value), format_, 1)
                 else:
                     value = self.visit(node.args, a)
@@ -128,12 +160,44 @@ class Runner(Visitor):
                     id_.value = int(inputs[i])
                 elif m == '%c':
                     id_.value = ord(inputs[i][0])
+                elif m == '%s':
+                    word = inputs[i]
+                    length = len(id_.symbols)
+                    for c in word:
+                        id_.symbols.put(length, id_.type_, None)
+                        id_.symbols.get(length).value = ord(c)
+                        length += 1
+        elif func == 'strlen':
+            a = args[0]
+            if isinstance(a, String):
+                return len(a.value)
+            elif isinstance(a, Id):
+                id_ = self.visit(node.args, a)
+                return len(id_.symbols)
+        elif func == 'strcat':
+            a, b = args[0], args[1]
+            dest = self.get_symbol(a)
+            values = []
+            if isinstance(b, Id):
+                src = self.get_symbol(b)
+                elems = [s.value for s in src.symbols]
+                non_nulls = [c for c in elems if c is not None]
+                values = [c for c in non_nulls]
+            elif isinstance(b, String):
+                values = [ord(c) for c in b.value]
+            i = len(dest.symbols)
+            for v in values:
+                dest.symbols.put(i, dest.type_, None)
+                dest.symbols.get(i).value = v
+                i += 1
         else:
             impl = self.global_[func]
+            self.call_stack.append(func)
             self.init_scope(impl.block)
             self.visit(node, node.args)
             result = self.visit(node, impl.block)
             self.clear_scope(impl.block)
+            self.call_stack.pop()
             self.return_ = False
             return result
 
@@ -141,6 +205,8 @@ class Runner(Visitor):
         result = None
         scope = id(node)
         self.scope.append(scope)
+        if len(self.local[scope]) > 5:
+            exit(0)
         for n in node.nodes:
             if self.return_:
                 break
@@ -163,15 +229,24 @@ class Runner(Visitor):
     def visit_Args(self, parent, node):
         func = parent.id_.value
         impl = self.global_[func]
+        scope = id(impl.block)
+        self.scope.append(scope)
         for p, a in zip(impl.params.params, node.args):
+            self.search_new_call = False
             arg = self.visit(impl.block, a)
+            self.search_new_call = True
             id_ = self.visit(impl.block, p.id_)
             id_.value = arg
             if isinstance(arg, Symbol):
                 id_.value = arg.value
+        self.scope.pop()
 
     def visit_Elems(self, parent, node):
-        pass
+        id_ = self.get_symbol(parent.id_)
+        for i, e in enumerate(node.elems):
+            value = self.visit(node, e)
+            id_.symbols.put(i, id_.type_, None)
+            id_.symbols.get(i).value = value
 
     def visit_Break(self, parent, node):
         pass
